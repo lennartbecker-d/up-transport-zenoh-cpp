abf1534b5ec0980ed70f992cc8458c97eafd5857
Lennart Becker <lennart.becker@d-fine.com>
2025-05-22 11:40:02 +0200

    Added logging

--- src/ZenohUTransport.cpp
+++ src/ZenohUTransport.cpp
@@ -241,6 +241,7 @@ v1::UStatus ZenohUTransport::registerPublishNotificationListener_(
 		auto maybe_message = sampleToUMessage(sample);
 		if (maybe_message.has_value()) {
 			listener(maybe_message.value());
+			spdlog::debug("Listener executed successfully. message:{}", maybe_message.value().DebugString());
 		} else {
 			spdlog::error("on_sample: failed to retrieve uMessage");
 		}
